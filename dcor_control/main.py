c5563d6cf85ecc88b4a6c3b5469ac5dec93a587b
Paul Müller <dev@craban.de>
2020-09-04 17:28:24 +0200

    cleanup

--- dcor_control/main.py
+++ dcor_control/main.py
@@ -11,22 +11,15 @@ from .server import status
 from .util import CKANINI
 
 
-def abort_if_false(ctx, param, value):
-    if not value:
-        ctx.abort()
-
-
 @click.group()
 def cli():
     pass
 
 
 @click.command()
-@click.option('--yes', is_flag=True, callback=abort_if_false,
-              expose_value=False,
-              prompt='Are you sure you want to reset your DCOR installation?')
-@click.pass_context
-def reset(ctx):
+@click.confirmation_option(
+    prompt="Are you sure you want to reset your DCOR installation?")
+def reset():
     """Reset DCOR database, webassets, and solr search index
 
     Before resetting the database, a dump is created in /backup/
@@ -53,8 +46,8 @@ def reset(ctx):
     click.secho("Reloading CKAN...", bold=True)
     sp.call("sudo supervisorctl reload", shell=True, stdout=sp.DEVNULL)
 
-    click.secho('DCOR reset: SUCCESS', fg=u'green', bold=True)
-    click.secho('Please delete resources yourself!', fg=u'green', bold=True)
+    click.secho('DCOR reset: SUCCESS - Please delete resources yourself!',
+                fg=u'green', bold=True)
 
 
 cli.add_command(inspect)
