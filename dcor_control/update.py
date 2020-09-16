23a1a8dbc418ccb2b9a5e83ea06b9d9850da7797
Paul Müller <dev@craban.de>
2020-09-16 18:56:10 +0200

    enh: update now also deals with git repositories

--- dcor_control/update.py
+++ dcor_control/update.py
@@ -1,3 +1,5 @@
+import os
+import pathlib
 import subprocess as sp
 import sys
 
@@ -16,11 +18,36 @@ def package_is_editable(name):
     return False
 
 
+def update_package(name):
+    for path_item in sys.path:
+        if name in path_item:
+            # This means that the package is probably installed
+            # in editable mode.
+            is_git = (pathlib.Path(path_item) / ".git").exists()
+            if is_git:
+                click.secho("Attempting to update git repository "
+                            + "at '{}'.".format(path_item), bold=True)
+                wd = os.getcwd()
+                os.chdir(path_item)
+                try:
+                    sp.check_output("git pull", shell=True)
+                except sp.CalledProcessError:
+                    click.secho("...failed!", bold=True)
+                finally:
+                    os.chdir(wd)
+                break
+    else:
+        click.secho("Updating package '{}' using pip...".format(name),
+                    bold=True)
+        sp.check_output("pip install --upgrade {}".format(name),
+                        shell=True)
+
+
 @click.command()
 @click.confirmation_option(
     prompt="Are you sure you want to update your DCOR installation?")
 def update():
-    """Update all DCOR CKAN extensions"""
+    """Update all DCOR CKAN extensions using pip/git"""
     for name in [
         "ckanext-dc_log_view",
         "ckanext-dc_serve",
@@ -31,13 +58,7 @@ def update():
         "dcor_shared",
         "dcor_control",
     ]:
-        if not package_is_editable(name):
-            click.secho("Updating '{}'...".format(name), bold=True)
-            sp.check_output("pip install --upgrade {}".format(name),
-                            shell=True)
-        else:
-            click.secho("Not updating '{}', because it looks".format(name)
-                        + "like an editable install.", bold=True)
+        update_package(name)
 
     click.secho("Reloading CKAN...", bold=True)
     sp.call("supervisorctl reload", shell=True, stdout=sp.DEVNULL)
