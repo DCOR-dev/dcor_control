b7c228c1000ce736bf36d830190ff8ded25fe506
Paul Müller <dev@craban.de>
2020-10-09 22:36:53 +0200

    enh: include Key ID and hostname in backup file names

--- dcor_control/backup.py
+++ dcor_control/backup.py
@@ -1,4 +1,5 @@
 import pathlib
+import socket
 import subprocess as sp
 import time
 
@@ -10,7 +11,9 @@ def db_backup():
     bpath = pathlib.Path("/backup") / time.strftime('%Y-%m')
     bpath.mkdir(parents=True, exist_ok=True)
     bpath.chmod(0o0500)
-    dpath = bpath / time.strftime('ckan_db_backup_%Y-%m-%d_%H-%M-%S.dump')
+    name = time.strftime('ckan_db_{}_%Y-%m-%d_%H-%M-%S.dump'.format(
+        socket.gethostname()))
+    dpath = bpath / name
     sp.check_output("sudo -u postgres pg_dump --format=custom "
                     + "-d ckan_default > {}".format(dpath), shell=True)
     assert dpath.exists()
@@ -57,6 +60,7 @@ def gpg_encrypt(path_in, path_out, key_id):
 def encrypted_database_backup(key_id):
     """Create an asymmetrically encrypted database backup on /data/"""
     dpath = db_backup()
-    eout = pathlib.Path("/data/encrypted_db_dumps/") / (dpath.name + ".gpg")
+    name = "{}_{}.gpg".format(dpath.name, key_id)
+    eout = pathlib.Path("/data/encrypted_db_dumps/") / dpath.parent.name / name
     gpg_encrypt(path_in=dpath, path_out=eout, key_id=key_id)
     click.secho("Created {}".format(eout), bold=True)
