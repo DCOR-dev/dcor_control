c038d5bd619d7566ab3bd3b6a80b91ed470a58e3
Paul Müller <dev@craban.de>
2024-01-26 23:25:37 +0100

    setup: pin uwsgi==2.0.21 due to non-working harakiri

--- setup.py
+++ setup.py
@@ -61,6 +61,9 @@ setup(
         "dcor_shared>=0.6.0",
         "importlib_resources",
         "numpy>=1.21",  # CVE-2021-33430
+        # https://github.com/unbit/uwsgi/issues/2580
+        # https://github.com/unbit/uwsgi/pull/2587
+        "uwsgi==2.0.21",
         ],
     # not to be confused with definitions in pyproject.toml [build-system]
     setup_requires=["pytest-runner"],
