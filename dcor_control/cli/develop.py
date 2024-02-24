787ac7a0e10128203444bae16c3045e7e415c55a
Paul Müller <dev@craban.de>
2024-02-25 00:37:16 +0100

    flake8

--- dcor_control/cli/develop.py
+++ dcor_control/cli/develop.py
@@ -15,8 +15,8 @@ from ..inspect import config_ckan, reload_supervisord
            "(CKAN extensions and helpers) to an editable install?")
 def develop():
     """Migrate all DCOR CKAN extensions to git-based editable installs"""
-    sp.check_output(f"pip install --upgrade pip", shell=True)
-    sp.check_output(f"pip install requests requests_toolbelt", shell=True)
+    sp.check_output("pip install --upgrade pip", shell=True)
+    sp.check_output("pip install requests requests_toolbelt", shell=True)
 
     for name in [
         "ckanext-dc_log_view",
