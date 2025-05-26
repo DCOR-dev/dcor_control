2d1ca606180fdb4784d8f78a57f761f0fcc1d198
Paul Müller <dev@craban.de>
2025-05-26 21:45:04 +0200

    flake8

--- tests/test_inspect_data_ckan.py
+++ tests/test_inspect_data_ckan.py
@@ -76,7 +76,8 @@ def test_get_dcor_site_config_dir(tmp_path):
     act_dir = inspect.get_dcor_site_config_dir()
     if act_dir is not None:
         # Only test this if relevant (Not set when managed remotely)
-        shutil.copy2(act_dir / "dcor_config.json", tmp_path / "dcor_config.json")
+        shutil.copy2(act_dir / "dcor_config.json",
+                     tmp_path / "dcor_config.json")
         try:
             os.environ["DCOR_SITE_CONFIG_DIR"] = str(tmp_path)
             assert str(inspect.get_dcor_site_config_dir()) == str(tmp_path)
